f47eebd4f991b842ee85f8887170be33e8c151a6
LiloFr <lilo.frich@gmail.com>
2023-03-16 14:23:52 +0300

    GroupsWin

---
+++ JournalApp/ViewModels/GroupsViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using JournalApp.Models;
+
+
+namespace JournalApp.ViewModels
+{
+    public class GroupsViewModel : DependencyObject
+    {
+
+
+        public string FilterText
+        {
+            get { return (string)GetValue(FilterTextProperty); }
+            set { SetValue(FilterTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for FilterText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FilterTextProperty =
+            DependencyProperty.Register("FilterText", typeof(string), typeof(GroupsViewModel), new PropertyMetadata("", TextFilterChanged));
+
+        private static void TextFilterChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            GroupsViewModel groupsViewModel = d as GroupsViewModel;
+            if (groupsViewModel != null)
+            {
+                groupsViewModel.Items.Filter = null;
+                groupsViewModel.Items.Filter = groupsViewModel.FilterGroup;
+            }
+        }
+
+        public ICollectionView Items
+        {
+            get { return (ICollectionView)GetValue(ItemsProperty); }
+            set { SetValue(ItemsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Items.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ItemsProperty =
+            DependencyProperty.Register("Items", typeof(ICollectionView), typeof(GroupsViewModel), new PropertyMetadata(null));
+
+        public GroupsViewModel()
+        {
+            List<Group> groups = new List<Group>();
+            Session.CurrentUser.Jurnal.ToList().ForEach(j => groups.Add(j.Group1));
+            Items = CollectionViewSource.GetDefaultView(groups);
+            Items.Filter = FilterGroup;
+        }
+
+        private bool FilterGroup(object obj)
+        {
+            Group group = obj as Group;
+            return group == null || string.IsNullOrWhiteSpace(FilterText) || group.Code.Contains(FilterText);
+        }
+    }
+}
--- JournalApp/Views/GroupsWin.xaml.cs
+++ JournalApp/Views/GroupsWin.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using JournalApp.Models;
+using JournalApp.ViewModels;
 
 
 namespace JournalApp.Views
@@ -25,35 +26,19 @@ namespace JournalApp.Views
         public GroupsWin()
         {
             InitializeComponent();
-
-            using(var db = new Entities())
-            {
-                Session.CurrentUser.Jurnal.ToList().ForEach(j => groups.Items.Add(j.Group1));
-            }
-        }
-
-        private void filterTextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            // очищаем листбокс
-            groups.Items.Clear();
-            // заполняем листбокс группами, которые подходят по условию
-            using (var db = new Entities())
-            {
-                Session.CurrentUser.Jurnal.ToList().ForEach(j => 
-                {
-                    if (j.Group1.Code.Contains(filterTextBox.Text))
-                        groups.Items.Add(j.Group1);
-                });
-            }
         }
 
         private void groups_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Session.CurrentGroup = groups.SelectedItem as Group;
             GroupWin groupWindow = new GroupWin();
-            Hide();
+            Close();
             groupWindow.ShowDialog();
-            Show();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = new GroupsViewModel();
         }
     }
 }
